2881a37de62be4d6822860f005b684d824c945ca
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-06 12:06:37 +0530

    calculating Win or Loss the game

--- GamblerSamulation/Gambler.cs
+++ GamblerSamulation/Gambler.cs
@@ -11,6 +11,16 @@ namespace GamblerSamulation
             int stake = 100;
             int bet = 1;
             int stakeValue;
+            int percent=50;
+            int winning = 0;
+            int lossing = 0;
+            
+            int value = (stake * percent) / 100;
+            winning = stake + value;
+            Console.WriteLine("The Winning value of gambler is : " + winning);
+
+            lossing = stake - value;
+            Console.WriteLine("The Lossing value of gambler is : " + lossing);
 
             Random ran = new Random();
             int toss = ran.Next(0, 2);
