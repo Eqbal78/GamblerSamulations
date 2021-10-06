5f8b0569ff2fec86a6762b9f9276b5220d65e85e
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-10-06 10:05:06 +0530

    Print EveryDay Stake and bet

---
+++ GamblerSamulation/Gambler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerSamulation
+{
+    class Gambler
+    {
+        public void Game()
+        {
+            int stake = 100;
+            int bet = 1;
+
+            Console.WriteLine("Every Day Stake is " + stake);
+            Console.WriteLine("EveryTime Bet is " + bet);
+        }
+    }
+}
